aa5f8364f9c37c89112e0cf58f6c9fb7eec91efd
tannerlsmith <tanner.smith@uvm.edu>
2021-09-14 22:52:55 -0400

    created layout for shopping cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,6 +6,7 @@ import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
+import Cart from '../components/Cart';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -63,6 +64,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
